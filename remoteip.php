63d6f296def84c2b450b6961116858c1f96e5cfe
Takashi SASAKI <takashi316@gmail.com>
2021-03-04 08:34:58 +0900

    show remote ip address

---
+++ remoteip.php
@@ -0,0 +1,27 @@
+<html>
+
+<head>
+    <title>Remote IP Address</title>
+    <style>
+        html {
+            margin: 0px;
+            padding: 0px;
+        }
+
+        body {
+            margin: 0px;
+            padding: 0px;
+        }
+
+        div {
+            margin: 0px;
+            padding: 0px;
+        }
+    </style>
+</head>
+
+<body>
+    <div>Your IP address is <?php echo $_SERVER["REMOTE_ADDR"]; ?>. Have fun.</div>
+</body>
+
+</html>
